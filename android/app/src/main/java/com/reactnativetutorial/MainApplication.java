2e1d08b2efb4934b8fb8461c8c2d070ba8d63d9f
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2018-07-12 16:30:36 +0900

    [WIP] checkpoint: build success

--- android/app/src/main/java/com/reactnativetutorial/MainApplication.java
+++ android/app/src/main/java/com/reactnativetutorial/MainApplication.java
@@ -9,6 +9,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new FabricPackage()
+        new FabricPackage(),
+        new MainReactPackage()
       );
     }
 
@@ -42,6 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
